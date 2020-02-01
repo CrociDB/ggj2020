24d4799596a9bcf0463b268ccad116316af33d43
Bruno Croci <crocidb@gmail.com>
2020-02-01 20:20:17 +0000

    added objective and improved level

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Objective : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject m_Model;
+
+    [Header("Tweak Values")]
+    public float m_FloatAmount;
+    public float m_FloatSpeed;
+    public float m_RotateSpeed;
+
+    public void Update()
+    {
+        m_Model.transform.Rotate(Vector3.up * m_RotateSpeed);
+        m_Model.transform.localPosition = Vector3.up * Mathf.Sin(Time.time * m_FloatSpeed) * m_FloatAmount;
+    }
+}
