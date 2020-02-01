17bd660df606fa9d1a628053a32ce9780dc56bc4
Bruno Croci <crocidb@gmail.com>
2020-02-01 15:04:02 +0000

    moving objects

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(Renderer))]
+public class MovableObject : MonoBehaviour
+{
+    public Material m_SelectedMaterial;
+
+    private Rigidbody m_Rigidbody;
+    private MeshFilter m_MeshFilter;
+    private Renderer m_Renderer;
+
+    private Material m_DefaultMaterial;
+
+    private bool m_Moving;
+    private Vector3 m_TargetPosition;
+    private float m_MovingForce;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_MeshFilter = GetComponent<MeshFilter>();
+        m_Renderer = GetComponent<Renderer>();
+
+        m_DefaultMaterial = m_Renderer.material;
+    }
+
+    public void Update()
+    {
+        if (m_Moving)
+        {
+            UpdatePosition();
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        var dir = m_TargetPosition - transform.position;
+        m_Rigidbody.AddForce(dir * m_MovingForce);
+    }
+
+    public void SelectObject()
+    {
+        m_Renderer.material = m_SelectedMaterial;
+        m_Rigidbody.useGravity = false;
+        m_Moving = true;
+    }
+
+    public void UnselectObject()
+    {
+        m_Renderer.material = m_DefaultMaterial;
+        m_Rigidbody.useGravity = true;
+        m_Moving = false;
+    }
+
+    public void UpdateTargetPosition(Vector3 targetPosition, float force)
+    {
+        m_TargetPosition = targetPosition;
+        m_MovingForce = force;
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveController : MonoBehaviour
+{
+    [Header("References")]
+    public Camera m_Camera;
+    public Image m_AimImage;
+    public LineRenderer m_LineRenderer;
+
+    [Header("Tweak values")]
+    public float m_RaycastDistance = 20.0f;
+    public float m_DistanceTargetMoving = 5.0f;
+    public float m_MovingForce;
+
+    private MovableObject m_SelectedObject;
+    private bool m_MovingObject;
+
+    public void Update()
+    {
+        var targetPosition = transform.position + m_Camera.transform.forward * m_DistanceTargetMoving;
+        UpdateLine(targetPosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (m_SelectedObject != null)
+            {
+                m_SelectedObject.SelectObject();
+                m_MovingObject = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (m_SelectedObject != null)
+            {
+                m_SelectedObject.UnselectObject();
+            }
+
+            m_MovingObject = false;
+        }
+
+        if (m_MovingObject)
+        {
+            m_SelectedObject.UpdateTargetPosition(targetPosition, m_MovingForce);
+        }
+        else
+        {
+            Ray r = new Ray(m_Camera.transform.position, m_Camera.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(r, out hit, m_RaycastDistance, (1 << 9)))
+            {
+                m_AimImage.color = Color.red;
+                m_SelectedObject = hit.collider.gameObject.GetComponent<MovableObject>();
+            }
+            else
+            {
+                m_AimImage.color = Color.white;
+                m_SelectedObject = null;
+            }
+        }
+    }
+
+    private void UpdateLine(Vector3 targetPosition)
+    {
+        m_LineRenderer.enabled = m_SelectedObject != null && m_MovingObject;
+        if (m_SelectedObject && m_MovingObject)
+        {
+            m_LineRenderer.SetPosition(0, transform.position);
+            m_LineRenderer.SetPosition(1, targetPosition);
+            m_LineRenderer.SetPosition(2, m_SelectedObject.transform.position);
+        }
+    }
+}
