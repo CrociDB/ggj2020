730ec6cf1c8305f860c597979a9fc7b7f3fc7264
Bruno Croci <crocidb@gmail.com>
2020-02-02 12:15:41 +0000

    created level4

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Wall m_Wall;
+
+    private SpringJoint m_Joint;
+
+    private void Awake()
+    {
+        m_Joint = GetComponentInChildren<SpringJoint>();
+    }
+
+    public void Update()
+    {
+        m_Wall.m_Open = m_Joint.currentForce.magnitude >= 0.93f;
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [Header("References")]
+    public Transform m_MovingPart;
+
+    [Header("Tweaks")]
+    public float m_OpenPosition;
+    public float m_ClosedPosition;
+
+    [Header("Movement")]
+    public bool m_Open = false;
+
+    public void Update()
+    {
+        var pos = m_MovingPart.localPosition;
+        if (m_Open)
+        {
+            pos.z = Mathf.Lerp(pos.z, m_OpenPosition, 5.0f * Time.deltaTime);
+        }
+        else
+        {
+            pos.z = Mathf.Lerp(pos.z, m_ClosedPosition, 5.0f * Time.deltaTime);
+        }
+        m_MovingPart.localPosition = pos;
+    }
+}
